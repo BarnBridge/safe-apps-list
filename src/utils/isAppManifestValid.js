f335cd012e5d13af1fec00b5d1a7d4ed15030bc3
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-26 12:00:18 +0800

    Delete src directory

--- src/utils/isAppManifestValid.js
+++
@@ -1,9 +0,0 @@
-module.exports = (appInfo) =>
-// `appInfo` exists and `name` exists
-!!appInfo?.name &&
-// if `name` exists is not 'unknown'
-appInfo.name !== 'unknown' &&
-// `description` exists
-!!appInfo.description &&
-// no `error` (or `error` undefined)
-!appInfo.error
--- src/write.js
+++
@@ -1,7 +0,0 @@
-const config = require('dotenv').config()
-const buildList = require('./buildList.js');
-
-(async () => {
-  const list = await buildList().then(list => JSON.stringify(list, null, 2))
-  console.log(list)
-})()
