68e84d15aa7c4901b7d205ff5ceb1a3cf3dac22a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-26 12:02:33 +0800

    Delete .eslintrc.js

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'prettier', // Add prettier rules to eslint
-    'plugin:prettier/recommended', // Plugin to use prettier rules with eslint
-  ]
-}
